92c2a0e83f256f50ef7c6be324b7af845f0db8f0
Martin B <m.bogdanov9110@gmail.com>
2023-05-21 15:13:38 +0300

    UPDATE:added Global Loader comp to App.tsx

--- space-explorer/src/App.tsx
+++ space-explorer/src/App.tsx
@@ -1,11 +1,21 @@
+import { useAppSelector } from "./App/hooks"
 import * as component from "./all-imported-components"
-
+import { globalState } from "./redux-slices/globalSlice"
 
 function App() {
 
+  const globalData = useAppSelector(globalState);
+
   return (
     <>
-      <component.NavBar />
+      < component.NavBar />
+      {
+        globalData.loading
+          ?
+          <component.GlobalLoader />
+          :
+          <div>Page loaded</div>
+      }
     </>
   )
 }
