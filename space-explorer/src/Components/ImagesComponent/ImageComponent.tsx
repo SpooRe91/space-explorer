cb5d489ea2eaa97df5ba44a530d4e16727a0ceec
Martin B <m.bogdanov9110@gmail.com>
2023-05-29 22:17:36 +0300

    added styles and imported ImageListItemBar from MUI

--- space-explorer/src/Components/ImagesComponent/ImageComponent.tsx
+++ space-explorer/src/Components/ImagesComponent/ImageComponent.tsx
@@ -1,4 +1,5 @@
 
+import { ImageListItemBar } from "@mui/material";
 import { TImageData } from "../../Interfaces and types/Types/types";
 import styles from './ImageComponent.module.scss';
 import { Link } from "react-router-dom";
@@ -9,12 +10,16 @@ const ImageComponent = (item: TImageData) => {
             <Link to={`${item?.links[0]?.href}`} rel="noopenner" target="_blank">
                 <img
                     src={item?.links[0]?.href}
-                    title={item?.data[0].title}
+
                     alt="#"
                 />
             </Link>
             <div className={styles["card-content"]}>
-                <p>
+                <ImageListItemBar className={styles["card-content-title"]}
+                    title={item?.data[0].title}
+                    position="below"
+                />
+                <p className={styles["card-content-description"]}>
                     {item?.data[0].description}
                 </p>
                 <button>Learn More</button>
