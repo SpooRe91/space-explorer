24bc53d5ff8ecafb9a53880e5ac27f2ea4de317c
Martin B <m.bogdanov9110@gmail.com>
2023-05-29 22:24:28 +0300

    UPDATE: fixed the fetch of Images

--- space-explorer/src/utils/Api.tsx
+++ space-explorer/src/utils/Api.tsx
@@ -1,3 +1,18 @@
 import axios from "axios";
 
 export const imagesApi = axios.create({ baseURL: 'https://images-api.nasa.gov/' });
+
+export const fetchImages = async () => {
+    try {
+        const res = await imagesApi.get(`search?q=solar%20system%20planets&media_type=image`);
+        if (res.status === 200) {
+            return await res.data.collection.items;
+        }
+    } catch (error: unknown) {
+        if (typeof error === "string") {
+            return error.toUpperCase()
+        } else if (error instanceof Error) {
+            return error.message
+        }
+    }
+};
