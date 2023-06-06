65a0e4dec16b62436e48929216487a63f02c5141
Martin B <m.bogdanov9110@gmail.com>
2023-06-06 03:31:08 +0300

    removed browser router

--- space-explorer/src/main.tsx
+++ space-explorer/src/main.tsx
@@ -4,16 +4,14 @@ import App from './App.tsx'
 import './styles/index.scss'
 import { Provider } from 'react-redux';
 import { store } from "./App/store";
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    {/* <BrowserRouter> */}
     <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
     </HashRouter>
-    {/* </BrowserRouter> */}
   </React.StrictMode>
 )
