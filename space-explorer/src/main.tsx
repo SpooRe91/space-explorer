f0e61ab68a8ab00d6104748870c6249c8283408d
Martin B <m.bogdanov9110@gmail.com>
2023-06-06 21:20:30 +0300

    UPDATE: -Switched to BrowerRouter

--- space-explorer/src/main.tsx
+++ space-explorer/src/main.tsx
@@ -4,14 +4,14 @@ import App from './App.tsx'
 import './styles/index.scss'
 import { Provider } from 'react-redux';
 import { store } from "./App/store";
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 )
