8ce29084877188627a9ee737edb09e715432dba9
Martin B <m.bogdanov9110@gmail.com>
2023-06-07 01:37:17 +0300

    NEW AboutPage.tsx: - Created REF for scrolling - Created IntersectionObserver for scrolling to add slide animation  NEW AboutPage scss:  -Created scss for AboutPage, and added styles for all elements  TODO:  -Add responsive styling

--- space-explorer/src/Pages/AboutPage/index.tsx
+++ space-explorer/src/Pages/AboutPage/index.tsx
@@ -1,33 +1,43 @@
 import { Link } from "react-router-dom";
 import styles from "./index.module.scss";
+import { useRef, useState } from "react";
 
 export const AboutPage = () => {
 
-    return (
-        //TODO: NEED TO STYLE THIS AND ADD SOME ADDITIONAL ELEMENTS!
-        <>
-            {/* <div className={styles["dark-bg"]} /> */}
-            <div className={styles["centered"]}
-                id="about"
-            >
-                <div className={styles["already-reg"]}>
-                    <h1 className={styles["already-reg"]}>About <span>"Space-explorer"</span></h1>
-                    <p className={styles["already-reg"]}>
-                        This is a personal project made with React, TypeScript, SCSS, ReactRouter, AXIOS, MaterialUi.
-                        Fan page of space exploration, different articles and images from the NASA APIs.
-                        If you enjoy the website, you can follow me on
-                        <Link to={'https://github.com/SpooRe91'}>
-                            GitHub
-                        </Link>
-                        and reach me on
-                        <Link to={"https://www.linkedin.com/in/mbogdanov9110/"}>
-                            LinkedIn
-                        </Link>
-                    </p>
-                </div>
+    const [isVisible, setIsVisible] = useState<boolean>(false);
 
+    const divRef = useRef<HTMLDivElement>(null);
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                setIsVisible(state => true);
+                observer.unobserve(entry.target);
+            }
+        }),
+        {
+            threshold: 1,
+        }
+    });
+
+    if (divRef.current)
+        observer.observe(divRef.current);
+
+    return (
+        <div id="about" ref={divRef} className={styles["about-container"]}>
+            <div className={styles[isVisible ? "slide-in" : "centered"]}>
+                <h2 className={styles["already-reg"]}>About <span>"Space-explorer"</span></h2>
+                <p className={styles["already-reg"]}>
+                    This is a personal project made with React, TypeScript, SCSS, ReactRouter, AXIOS, MaterialUi.
+                    Fan page of space exploration, different articles and images from the NASA APIs.
+                    If you enjoy the website, you can follow me on
+                    <Link to={'https://github.com/SpooRe91'}> GitHub </Link>
+                    and reach me on
+                    <Link to={"https://www.linkedin.com/in/mbogdanov9110/"}> LinkedIn </Link>
+                </p>
             </div>
-        </>
+            {/* TODO: NEED TO STYLE THIS AND ADD SOME ADDITIONAL ELEMENTS! */}
+        </div>
+
     );
 }
 export default AboutPage;
