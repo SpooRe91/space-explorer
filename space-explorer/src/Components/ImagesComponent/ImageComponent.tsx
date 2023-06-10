6d6e79761ef06b5259d7ac8f2c47805248949613
Martin B <m.bogdanov9110@gmail.com>
2023-06-10 04:22:02 +0300

    UPDATE: -Removed the link from the Images; Fixed some stylings

--- space-explorer/src/Components/ImagesComponent/ImageComponent.tsx
+++ space-explorer/src/Components/ImagesComponent/ImageComponent.tsx
@@ -1,24 +1,36 @@
 import { TImageData } from "../../Interfaces and types/Types/types";
+import { GlobalLoader } from "../../all-imported-components";
 import styles from './ImageComponent.module.scss';
-import { Link } from "react-router-dom";
+
+import { useAppDispatch } from "../../App/hooks";
+import { setToExpandImage } from "../../redux-slices/globalSlice";
+
 
 const ImageComponent = (item: TImageData) => {
 
+    const dispatch = useAppDispatch();
+
     return (
-        <div className={styles["card-component"]}>
-            <Link to={`${item?.links[0]?.href}`} rel="noopenner" target="_blank">
-                <img
-                    loading="lazy"
-                    src={item?.links[0]?.href}
-                    alt="../../assets/icons/img not found.png"
-                />
-            </Link>
+        <div
+            onClick={() => dispatch(setToExpandImage({ bool: true, href: item?.links[0]?.href, title: item?.data[0].title }))}
+            className={styles["card-component"]} >
+            {
+                item?.links[0]?.href
+                    ?
+                    <img
+                        loading="lazy"
+                        src={item?.links[0]?.href}
+                        alt="../../assets/icons/img not found.png"
+                    />
+                    :
+                    <GlobalLoader />
+            }
             <div className={styles["card-content"]}>
                 <p className={styles["card-content-title"]}>
                     {item?.data[0].title}
                 </p>
             </div>
-        </div>
+        </div >
     );
 };
 
