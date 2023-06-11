3374078ff07ffbd21ec54687e1eaf6cd3c3ff804
Martin B <m.bogdanov9110@gmail.com>
2023-06-11 22:08:54 +0300

    UPDATE: - Added lazy loading in all imports file - Added onLoad to scroll to the hash route element

--- space-explorer/src/App.tsx
+++ space-explorer/src/App.tsx
@@ -1,23 +1,32 @@
-import { useAppSelector } from "./App/hooks";
+import { useAppSelector } from "./App/hooks"
 import { globalState } from "./redux-slices/globalSlice";
 
 import * as component from "./all-imported-components";
+import { Suspense } from "react";
+import { useLocation } from "react-router";
 
 function App() {
 
   const globalData = useAppSelector(globalState);
+  const { hash } = useLocation();
 
   return (
-    <div className="main">
+    <div className="main"
+      onLoad={() => hash
+        ? document.querySelector(hash)?.scrollIntoView()
+        : null}
+    >
       <component.NavBar />
       {
         globalData.loading
           ?
           <component.GlobalLoader />
           : <>
-            <component.HomePage />
-            <component.ImagePage />
-            <component.AboutPage />
+            <Suspense>
+              <component.HomePage />
+              <component.ImagePage />
+              <component.AboutPage />
+            </Suspense>
           </>
       }
     </div>
--- space-explorer/src/all-imported-components.tsx
+++ space-explorer/src/all-imported-components.tsx
@@ -2,16 +2,22 @@ import ImageComponent from "./Components/ImagesComponent/ImageComponent";
 import NavBar from "./Layouts/AppNavBar/NavBar";
 import SideBar from "./Layouts/AppNavBar/SideBar/SideBar";
 import GlobalLoader from "./Layouts/GlobalLoader/GlobalLoader";
-import HomePage from "./Pages/HomePage";
-import AboutPage from "./Pages/AboutPage";
-import ImagePage from "./Pages/ImagePage/Index";
+import ImageModal from "./Components/ImageModal/ImageModal";
+import { lazy } from "react";
+import ErrorMessage from "./Layouts/ErrorMessage/ErrorMessage";
+
+const HomePage = lazy(() => import('./Pages/HomePage/index'));
+const ImagePage = lazy(() => import('./Pages/ImagePage/index'));
+const AboutPage = lazy(() => import('./Pages/AboutPage/index'));
 
 export {
+    HomePage,
+    ImagePage,
+    AboutPage,
     NavBar,
     GlobalLoader,
     SideBar,
-    HomePage,
-    ImagePage,
     ImageComponent,
-    AboutPage,
-}
+    ImageModal,
+    ErrorMessage
+};
