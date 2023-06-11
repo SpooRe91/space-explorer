6e83f0abd1507ac2e7483a13b7b2f60d2e542edb
Martin B <m.bogdanov9110@gmail.com>
2023-06-11 22:18:25 +0300

    NOTHING NEW: -Just tried adding request for PIC OF THE DAY, but NASA server was down TODO: - finish the above

--- space-explorer/src/utils/Api.tsx
+++ space-explorer/src/utils/Api.tsx
@@ -17,3 +17,19 @@ export const fetchImages = async (signal: AbortSignal, controller: AbortControll
         }
     }
 };
+
+// export const fetchImageOfTheDay = async (signal: AbortSignal, controller: AbortController) => {
+//     try {
+//         const res = await imagesApi.get(`search?q=solar%20system%20planets&media_type=image`, { signal: signal });
+//         if (res.status === 200) {
+//             return await res.data.collection.items;
+//         }
+//     } catch (error: unknown) {
+//         if (typeof error === "string") {
+//             return error.toUpperCase()
+//         } else if (error instanceof Error) {
+//             if (controller.signal.aborted) { return }
+//             return error.message
+//         }
+//     }
+// };
