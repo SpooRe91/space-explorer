eb92a4809f3e4cf1a71abb4985fecb71e9e55bf7
Martin B <m.bogdanov9110@gmail.com>
2023-06-11 22:14:43 +0300

    UPDATE on AboutPage: - CSS: Added mixins and styles for active and not-active -TSX: Added logic and used customIntersect hook, ot detect and change class if active or not

--- space-explorer/src/Pages/AboutPage/index.tsx
+++ space-explorer/src/Pages/AboutPage/index.tsx
@@ -1,30 +1,16 @@
 import { Link } from "react-router-dom";
 import styles from "./index.module.scss";
-import { useRef, useState } from "react";
+import { useRef } from "react";
+import useIntersectionHook from "../../customHooks/useIntersectionHook";
 
 export const AboutPage = () => {
 
-    const [isVisible, setIsVisible] = useState<boolean>(false);
-
     const divRef = useRef<HTMLDivElement>(null);
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                setIsVisible(true);
-                observer.unobserve(entry.target);
-            }
-        }),
-        {
-            threshold: 1,
-        }
-    });
-
-    if (divRef.current)
-        observer.observe(divRef.current);
+    const isActive = useIntersectionHook(divRef, 'aboutPage');
 
     return (
         <div id="about" ref={divRef} className={styles["about-container"]}>
-            <div className={styles[isVisible ? "slide-in" : "centered"]}>
+            <div className={styles[isActive ? "slide-in" : "slide-out"]}>
                 <h2 className={styles["already-reg"]}>About <span>"Space-explorer"</span></h2>
                 <p className={styles["already-reg"]}>
                     This is a personal project made with React, TypeScript, SCSS, ReactRouter, AXIOS, MaterialUi.
@@ -35,7 +21,6 @@ export const AboutPage = () => {
                     <Link to={"https://www.linkedin.com/in/mbogdanov9110/"}> LinkedIn </Link>
                 </p>
             </div>
-            {/* TODO: NEED TO STYLE THIS AND ADD SOME ADDITIONAL ELEMENTS! */}
         </div>
 
     );
