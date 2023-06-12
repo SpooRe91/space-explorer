25a12988bffd49d7e89f9a18626fcf6cd497c2fb
Martin B <m.bogdanov9110@gmail.com>
2023-06-13 01:06:19 +0300

    UPDATE: - Added comments to the useIntersectionHook

--- space-explorer/src/customHooks/useIntersectionHook.tsx
+++ space-explorer/src/customHooks/useIntersectionHook.tsx
@@ -10,28 +10,32 @@ const useIntersectionHook = (tragetRef: React.RefObject<HTMLElement>,
     const dispatch = useAppDispatch();
 
     useEffect(() => {
-
+        // CREATING NEW OBSERVER FOR INTERSECTION
         const observer = new IntersectionObserver((entries) => {
+            // GRABS THE ENTRY AND CHECKS IF IT INTERSECTS
             const [entry] = entries;
             setIsActive(entry.isIntersecting);
+            // IF INTERSECTS - DISPATCHING THE ACTIVE ELEMENT'S NAME AND BOOLEAN TO INDICATE IT;
             if (entry.isIntersecting && tragetRef.current) {
                 dispatch(setActiveNavElement({
                     isActive: true,
                     activeEl: pageName
-                }))
+                }));
+                // IF IT'S NOT INTERSECTING - DISPATCHING THE ACTIVE BOOLEAN TO FALSE AND ACTIVE ELEMENT TO "";
             } else {
                 dispatch(setActiveNavElement({
                     isActive: false,
                     activeEl: ""
-                }))
+                }));
             }
         },
             { threshold: 1 }
         );
-
+        // HERE WE SAY THAT IF WE HAVE A REF TO A SPECIFIC EL - WE SHOULD OBSERVE IT;
         if (tragetRef.current) {
             observer.observe(tragetRef.current);
         }
+        // AND IF THE CURRENT ELEMENT IS ACTIVE - THEN WE SHOULD STOP OBSERVING IT;
         if (isActive && tragetRef.current) {
             observer.unobserve(tragetRef.current);
         }
