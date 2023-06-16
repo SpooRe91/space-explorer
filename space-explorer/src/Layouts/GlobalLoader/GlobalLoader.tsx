4722d3712b029c26e6ba20fbb1b201eefcece173
Martin B <m.bogdanov9110@gmail.com>
2023-06-16 20:59:39 +0300

    GlobalLoader: \n -changed loader to BarLoader \n -removed most of the css for previous loader

--- space-explorer/src/Layouts/GlobalLoader/GlobalLoader.tsx
+++ space-explorer/src/Layouts/GlobalLoader/GlobalLoader.tsx
@@ -1,17 +1,13 @@
-import { RingLoader } from 'react-spinners';
+import BarLoader from "react-spinners/BarLoader";
 import styles from './GlobalLoader.module.scss';
 
 const GlobalLoader = () => {
     return (
         <div className={styles['global-loader-container']}>
-            <div className={styles['global-loader-single-container-loading']}>
-                <p className={styles['global-loader-text']}>
-                    Loading...
-                </p>
-                <RingLoader className={styles['global-loader-spinner']}
-                    size={100}
-                    color='#36d7b7' />
-            </div>
+            <BarLoader
+                width={"100%"}
+                height={5}
+                color='#1e4d97' />
         </div>
     )
 }
