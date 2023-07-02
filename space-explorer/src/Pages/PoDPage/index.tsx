acbe3348cd4c43cbe26805542d27af98c015f5aa
Martin B <m.bogdanov9110@gmail.com>
2023-07-02 22:11:37 +0300

    UPDATE PODPage: -UPdated logic for PODpage -Removed styling from PODPage UPDATE:Main SCC: - Added z-index to global loader container to be 9999

--- space-explorer/src/Pages/PoDPage/index.tsx
+++ space-explorer/src/Pages/PoDPage/index.tsx
@@ -1,30 +1,20 @@
-import { useEffect } from "react";
-import { useAppDispatch } from "../../App/hooks";
-
 import styles from "./index.module.scss";
 
-import { fetchImageOfTheDay } from "../../utils/Picture-of-the-day-api";
-import { setError, setIsLoading } from "../../redux-slices/globalSlice";
-import { setPodData } from "../../redux-slices/PODslice";
-
 import PoDModal from "../../Components/PoDModal/PoDModal";
+import useGetPod from "../../customHooks/useGetPod";
+import { useAppSelector } from "../../App/hooks";
+import { podState } from "../../redux-slices/PODslice";
+import { useEffect } from "react";
 
 const PoDPage = () => {
 
-    const controller = new AbortController();
-    const { signal } = controller;
-
-    const dispatch = useAppDispatch();
+    const podData = useAppSelector(podState);
+    const podResult = useGetPod();
 
     useEffect(() => {
         (async function () {
-            const data = await fetchImageOfTheDay(signal, controller);
-            dispatch(setIsLoading(true));
-
-            typeof data === typeof { data }
-                ? (dispatch(setIsLoading(false)), dispatch(setPodData(data)))
-                : (dispatch(setIsLoading(false)), dispatch(setError(data)))
-        })();
+            await podResult(podData.prevUrl);
+        })()
     }, [])
 
     return (
@@ -33,5 +23,4 @@ const PoDPage = () => {
         </div>
     )
 }
-
 export default PoDPage
