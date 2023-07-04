59660794db961e7d1fc88697bf0232fb6fa0d2f8
Martin B <m.bogdanov9110@gmail.com>
2023-07-05 00:06:51 +0300

    NEW FEATURES ImagePage: -Added load more images feature -Added error handling and visibility -Added logic for handling new search -Added logic to remove previous search -Added logic to remove previous data loaded

--- space-explorer/src/Pages/ImagePage/Index.tsx
+++ space-explorer/src/Pages/ImagePage/Index.tsx
@@ -1,8 +1,8 @@
 
-import { useEffect, useRef } from "react";
+import { ChangeEvent, FormEvent, useRef, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../App/hooks";
 import { globalState, setError, setIsLoading } from "../../redux-slices/globalSlice";
-import { imageState, setImageData } from "../../redux-slices/imagesSlice";
+import * as imageSlice from "../../redux-slices/imagesSlice";
 import { ImageListItem } from "@mui/material";
 import { fetchImages } from "../../utils/Api";
 
@@ -14,7 +14,7 @@ import useIntersectionHook from "../../customHooks/useIntersectionHook";
 
 const ImagePage = () => {
 
-    const imageData = useAppSelector(imageState);
+    const imageData = useAppSelector(imageSlice.imageState);
     const globalData = useAppSelector(globalState);
     const dispatch = useAppDispatch();
 
@@ -24,17 +24,86 @@ const ImagePage = () => {
     const divRef = useRef<HTMLDivElement>(null);
 
     const isActive = useIntersectionHook(divRef, '#gallery');
+    const [searchValue, setSearchValue] = useState<string>('');
+    const [disableButton, setToDisableButton] = useState<boolean>(false);
 
-    useEffect(() => {
-        (async function () {
-            const data = await fetchImages(signal, controller);
-            dispatch(setIsLoading(true));
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setToDisableButton(false);
+        dispatch(imageSlice.setClearImageData(true));
+        dispatch(imageSlice.setImageQueryString(searchValue));
+        if (searchValue === '') {
+            dispatch(setIsLoading(false));
+            dispatch(setError('Please enter your search first!'));
+            dispatch(imageSlice.setImagePage(1));
+            return;
+        }
 
-            typeof data === typeof [data]
-                ? (dispatch(setIsLoading(false)), dispatch(setImageData(data)))
-                : (dispatch(setIsLoading(false)), dispatch(setError(data)))
-        })();
-    }, [])
+        dispatch(setIsLoading(true));
+        const data = await fetchImages(signal, controller, imageData.imagePage, searchValue);
+        setSearchValue('');
+
+        if (data.length && typeof data !== "string") {
+            dispatch(setIsLoading(false));
+            dispatch(imageSlice.setImageData(data));
+            dispatch(imageSlice.setImagePage(imageData.imagePage + 1));
+            return;
+        }
+
+        if (!data.length && typeof data !== "string") {
+            dispatch(setIsLoading(false));
+            dispatch(imageSlice.setImagePage(1));
+            dispatch(setIsLoading(false)), dispatch(setError("Sorry, no results found!"));
+            return;
+
+        } else {
+            dispatch(setIsLoading(false));
+            dispatch(imageSlice.setImagePage(1));
+            dispatch(setIsLoading(false)), dispatch(setError(data));
+            return;
+        }
+
+
+    };
+
+    const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(e.currentTarget.value);
+        dispatch(setError(''));
+        dispatch(imageSlice.setImagePage(1));
+    }
+
+    const handlePageChange = async () => {
+
+        dispatch(setIsLoading(true));
+        dispatch(imageSlice.setImagePage(imageData.imagePage + 1));
+
+        const data = await fetchImages(signal, controller, imageData.imagePage, imageData.queryString);
+
+        if (data.length && typeof data !== "string") {
+            dispatch(setIsLoading(false));
+            dispatch(imageSlice.setAdditionalImageData(data));
+            dispatch(imageSlice.setImagePage(imageData.imagePage + 1));
+            return;
+        }
+
+        if (!data.length && typeof data !== "string") {
+            if (imageData.imagePage) {
+                setToDisableButton(true);
+                dispatch(setIsLoading(false));
+                return;
+            }
+            dispatch(imageSlice.setImagePage(1));
+            dispatch(setIsLoading(false)), dispatch(setError("Sorry, no results found!"));
+            return;
+
+        } else {
+            dispatch(setIsLoading(false));
+            dispatch(imageSlice.setImagePage(1));
+            dispatch(setIsLoading(false)), dispatch(setError(data));
+            return;
+        }
+
+    }
 
     return (
         <section id="gallery" className={styles[isActive
@@ -43,6 +112,22 @@ const ImagePage = () => {
             <div ref={divRef} className={styles["image-container-heading-container"]}>
                 <h1>Gallery</h1>
             </div>
+            <form
+                className={styles['search-form']}
+                onSubmit={(e) => handleSubmit(e)}>
+                <label
+                    className={styles["form-label"]}
+                    htmlFor="search-images">
+                    Search images:
+                </label>
+                <input
+                    className={styles["form-input"]}
+                    onChange={(e) => handleInput(e)}
+                    type="text"
+                    name="search-images"
+                    value={searchValue} />
+                <input type="submit" value="search" className={styles["form-submit-button"]} />
+            </form>
             {
                 // IF MODAL IS TO BE SHOWN
                 globalData.toExpandImage
@@ -52,31 +137,38 @@ const ImagePage = () => {
             }
             {
                 // IF THERE ARE IMAGES IN THE GLOBAL DATA, SHOW THE LIST OF IMAGES
-                imageData.allData[0].href !== ''
+
+                globalData.error
                     ?
-                    <div className={styles[isActive
-                        ? "image-list"
-                        : "image-list-not-active"
-                    ]}>
-                        {imageData.allData.map((item: TImageData) => (
-                            <ImageListItem key={item.data[0].nasa_id} >
-                                <component.ImageComponent {...item} />
-                            </ImageListItem>
-                        ))}
+                    <div className={styles['loader-error']}>
+                        <component.ErrorMessage error={globalData.error} />
                     </div>
                     :
-                    // IF THERE ARE NO IMAGES, CHECK IF IT'S STILL LOADING OR IF THERE IS AN ERROR
-                    <div className={styles['loader-error']}>
-                        {
-                            globalData.loading
-                                ? <component.GlobalLoader />
-                                : globalData.error
-                                    ? <component.ErrorMessage error={globalData.error} />
-                                    : null
-                        }
-                    </div>
-            }
+                    imageData?.allData[0]?.href
+                        ?
+                        <div className={styles[isActive
+                            ? "image-list"
+                            : "image-list-not-active"
+                        ]}>
+                            {imageData.allData.map((item: TImageData) => (
+                                <ImageListItem key={item.data[0].nasa_id} >
+                                    <component.ImageComponent {...item} />
+                                </ImageListItem>
+                            ))}
 
+                            <button
+                                disabled={disableButton}
+                                onClick={() => handlePageChange()}>
+                                {
+                                    disableButton
+                                        ? 'NO more images to load'
+                                        : 'Fetch more images'
+                                }
+                            </button>
+                        </div>
+                        : null
+                // IF THERE ARE NO IMAGES, CHECK IF IT'S STILL LOADING OR IF THERE IS AN ERROR
+            }
         </section >
     )
 }
