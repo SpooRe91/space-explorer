6582e108f54b912957ec6f9e9216671a71f40070
Martin B <m.bogdanov9110@gmail.com>
2023-07-05 00:03:40 +0300

    UPDATE ErrorMessage.tsx: -Added some conditional rendering to show the proper error

--- space-explorer/src/Layouts/ErrorMessage/ErrorMessage.tsx
+++ space-explorer/src/Layouts/ErrorMessage/ErrorMessage.tsx
@@ -1,18 +1,25 @@
 import Alert from '@mui/material/Alert';
-import { useAppSelector } from '../../App/hooks';
-import { globalState } from '../../redux-slices/globalSlice';
+import { useAppDispatch, useAppSelector } from '../../App/hooks';
+import { globalState, setError } from '../../redux-slices/globalSlice';
 
 const ErrorMessage = ({ error }: { error: string }) => {
 
     const globalData = useAppSelector(globalState);
+    const dispatch = useAppDispatch();
     globalData.error
     return (
         <div>
             <Alert style={{ fontSize: "1.5rem" }}
                 severity="error">
                 {/* IF THERE IS AN ERORR PASSED - DISPLAY THAT, OTHERWISE DISPLAY THE GLOBAL ERROR */}
-                Sorry, there seems to be a {error ? error : globalData.error}, please try again later!
+                Error: {
+                    globalData.error === "Network Error"
+                        ? 'Sorry, there is a network issue, please try again later!'
+                        : error
+                            ? error
+                            : globalData.error}
             </Alert>
+            <button onClick={() => dispatch(setError(''))}>X</button>
         </div>
     )
 }
