635bd6c297b8bf7d00b646544d97006337841b99
Martin B <m.bogdanov9110@gmail.com>
2023-07-05 00:10:11 +0300

    UPDATE fetchImagesApi: -Added page number and queryString as arguments

--- space-explorer/src/utils/Fetch-search-images.api.tsx
+++ space-explorer/src/utils/Fetch-search-images.api.tsx
@@ -2,9 +2,9 @@ import axios from "axios";
 
 export const imagesApi = axios.create({ baseURL: 'https://images-api.nasa.gov/' });
 
-export const fetchImages = async (signal: AbortSignal, controller: AbortController) => {
+export const fetchImages = async (signal: AbortSignal, controller: AbortController, page: number, queryString: string) => {
     try {
-        const res = await imagesApi.get(`search?q=solar%20system%20planets&media_type=image`, { signal: signal });
+        const res = await imagesApi.get(`search?q=${queryString}&media_type=image&page_size=40&page=${page}`, { signal: signal });
         if (res.status === 200) {
             return await res.data.collection.items;
         }
