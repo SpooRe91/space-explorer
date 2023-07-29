549f67d71726e6e66a304259e794e1e41bd9c067
Martin B <m.bogdanov9110@gmail.com>
2023-07-29 15:30:16 +0300

    DELETE SIDEBAR: Deleted the drop down nav

--- space-explorer/src/all-imported-components.tsx
+++ space-explorer/src/all-imported-components.tsx
@@ -1,6 +1,5 @@
 import ImageComponent from "./Components/ImagesComponent/ImageComponent";
 import NavBar from "./Layouts/AppNavBar/NavBar";
-import SideBar from "./Layouts/AppNavBar/SideBar/SideBar";
 import GlobalLoader from "./Layouts/GlobalLoader/GlobalLoader";
 import ImageModal from "./Components/ImageModal/ImageModal";
 import ErrorMessage from "./Layouts/ErrorMessage/ErrorMessage";
@@ -13,17 +12,16 @@ import ArticlesPage from "./Pages/ArticlesPage";
 import ArticleCard from "./Components/ArticleCard/ArticleCard";
 
 export {
-    NavBar,
-    HomePage,
-    ImagePage,
-    AboutPage,
-    ArticlesPage,
-    GlobalLoader,
-    SideBar,
-    ImageComponent,
-    ImageModal,
-    ErrorMessage,
-    PoDPage,
-    PoDModal,
-    ArticleCard
+  NavBar,
+  HomePage,
+  ImagePage,
+  AboutPage,
+  ArticlesPage,
+  GlobalLoader,
+  ImageComponent,
+  ImageModal,
+  ErrorMessage,
+  PoDPage,
+  PoDModal,
+  ArticleCard,
 };
