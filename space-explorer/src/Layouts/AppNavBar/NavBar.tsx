a9b7797c763f7111c4bcb7cd75a5ea144e55e048
Martin B <m.bogdanov9110@gmail.com>
2023-08-03 21:44:56 +0300

    NEW: -Created custom hook to control the showing or hiding of nav bar on scroll Update: NavBar: -Implemented showing and hiding of nav when scrolling (non-mobile) -FIxed some styling for NavBar to properly behave on certain aspect ratios

--- space-explorer/src/Layouts/AppNavBar/NavBar.tsx
+++ space-explorer/src/Layouts/AppNavBar/NavBar.tsx
@@ -10,18 +10,28 @@ import {
 
 import styles from "./NavBar.module.scss";
 import icon from "../../assets/icons/android-chrome-192x192.png";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import useHideNavOnScroll from "../../customHooks/useHideNavOnScroll";
 
 const NavBar = () => {
   const globalData = useAppSelector(globalState);
   const dispatch = useAppDispatch();
   const { pathname, hash, key } = useLocation();
+  const [scrollNavUp, setToScrollNavUp] = useState<boolean>(false);
+
+  const checkIfMobile = (): void => {
+    if (screen.width <= 766) {
+      dispatch(setShowNav(!globalData.showSideNav));
+    }
+  };
 
   useEffect(() => {
     if (pathname !== "") {
       hash.replace(hash, "");
     }
+
     screen.width > 766 ? dispatch(setShowNav(true)) : "";
+
     if (hash) {
       setTimeout(() => {
         const element = document.querySelector(hash);
@@ -30,123 +40,131 @@ const NavBar = () => {
           element?.scrollIntoView();
         }
         if (element?.id === "home") {
-          element?.scrollTo({ top: 160, left: 0 });
+          window?.scrollTo({ top: 160, left: 0 });
         }
       }, 0);
     }
+
     return () => {
       clearTimeout(0);
     };
   }, [pathname, hash, key, dispatch]);
 
-  const checkIfMobile = (): void => {
-    if (screen.width <= 766) {
-      dispatch(setShowNav(!globalData.showSideNav));
-    }
-  };
+  //CUSTOM HOOK TO CONTROL THE SHOWING AND HIDING OF THE NAV BAR on SCROLL
+  useHideNavOnScroll(setToScrollNavUp);
+
 
   return (
-    <div className={styles["main-nav-container"]}>
-      <div className={styles["nav-logo-container"]}>
-        <NavLink
-          to={"/#home"}
-          className={styles["nav-logo-item"]}
-          onClick={() => dispatch(setToShowPoD(false))}
+    <section
+      className={styles["hiding-nav"]}
+      style={
+        scrollNavUp
+          ? { transform: "translateY(-100%)" }
+          : { transform: "translateY(0%)" }
+      }
+    >
+      <div className={styles["main-nav-container"]}>
+        <div className={styles["nav-logo-container"]}>
+          <NavLink
+            to={"/#home"}
+            className={styles["nav-logo-item"]}
+            onClick={() => dispatch(setToShowPoD(false))}
+          >
+            <img src={icon} alt="logo" />
+            Space Explorer
+          </NavLink>
+        </div>
+        <nav
+          className={styles["secondary-nav-container"]}
+          style={
+            globalData.showSideNav
+              ? { transform: "translateY(0%)" }
+              : { transform: "translateY(-100%)" }
+          }
         >
-          <img src={icon} alt="logo" />
-          Space Explorer
-        </NavLink>
+          <ul className={styles["nav-links"]} role="list">
+            <li className={styles["nav-link-item"]}>
+              <NavLink
+                aria-label="Home"
+                to={"/#home"}
+                onClick={() => dispatch(setToShowPoD(false))}
+                className={() =>
+                  globalData.activeNavElement.activeEl.includes("home")
+                    ? styles["active"]
+                    : styles["pending"]
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className={styles["nav-link-item"]}>
+              <NavLink
+                aria-label="Gallery"
+                to={"/#gallery"}
+                onClick={() => dispatch(setToShowPoD(false))}
+                className={() =>
+                  globalData.activeNavElement.activeEl.includes("gallery")
+                    ? styles["active"]
+                    : styles["pending"]
+                }
+              >
+                Gallery
+              </NavLink>
+            </li>
+            <li className={styles["nav-link-item"]}>
+              <NavLink aria-label="Articles" to={"/#articles"}
+                onClick={() => dispatch(setToShowPoD(false))}
+                className={() =>
+                  globalData.activeNavElement.activeEl.includes("articles")
+                    ? styles["active"]
+                    : styles['pending']
+                }>
+                Articles
+              </NavLink>
+            </li>
+            <li className={styles["nav-link-item"]}>
+              <NavLink
+                aria-label="About"
+                to={"/#about"}
+                onClick={() => dispatch(setToShowPoD(false))}
+                className={() =>
+                  globalData.activeNavElement.activeEl.includes("about")
+                    ? styles["active"]
+                    : styles["pending"]
+                }
+              >
+                About
+              </NavLink>
+            </li>
+          </ul>
+          <div className={styles["nav-navbar-collapse"]}>
+            <Link
+              to={"https://www.nasa.gov/"}
+              className={styles["nav-item-nasa"]}
+              rel="noreferrer"
+              target="_blank"
+            >
+              NASA
+            </Link>
+            <button
+              className={styles["nav-item-pod"]}
+              onClick={() => [
+                dispatch(setToShowPoD(!globalData.showPoD)),
+                dispatch(setIsLoading(true)),
+              ]}
+            >
+              Picture of the day
+            </button>
+          </div>
+          <div
+            className={styles["nav-toggler-contaier"]}
+            onClick={() => checkIfMobile()}
+          >
+            <BiDotsHorizontalRounded />
+          </div>
+        </nav>
       </div>
-      <nav
-        className={styles["secondary-nav-container"]}
-        style={
-          globalData.showSideNav
-            ? { transform: "translateY(0%)" }
-            : { transform: "translateY(-100%)" }
-        }
-      >
-        <ul className={styles["nav-links"]} role="list">
-          <li className={styles["nav-link-item"]}>
-            <NavLink
-              aria-label="Home"
-              to={"/#home"}
-              onClick={() => dispatch(setToShowPoD(false))}
-              className={() =>
-                globalData.activeNavElement.activeEl.includes("home")
-                  ? styles["active"]
-                  : styles["pending"]
-              }
-            >
-              Home
-            </NavLink>
-          </li>
-          <li className={styles["nav-link-item"]}>
-            <NavLink
-              aria-label="Gallery"
-              to={"/#gallery"}
-              onClick={() => dispatch(setToShowPoD(false))}
-              className={() =>
-                globalData.activeNavElement.activeEl.includes("gallery")
-                  ? styles["active"]
-                  : styles["pending"]
-              }
-            >
-              Gallery
-            </NavLink>
-          </li>
-          {/* <li className={styles["nav-link-item"]}>
-                        <NavLink aria-label="Articles" to={"/#articles"}
-                            onClick={() => dispatch(setToShowPoD(false))}
-                            className={() =>
-                                globalData.activeNavElement.activeEl.includes("articles")
-                                    ? styles["active"]
-                                    : styles['pending']
-                            }>
-                            Articles
-                        </NavLink>
-                    </li> */}
-          <li className={styles["nav-link-item"]}>
-            <NavLink
-              aria-label="About"
-              to={"/#about"}
-              onClick={() => dispatch(setToShowPoD(false))}
-              className={() =>
-                globalData.activeNavElement.activeEl.includes("about")
-                  ? styles["active"]
-                  : styles["pending"]
-              }
-            >
-              About
-            </NavLink>
-          </li>
-        </ul>
-        <div className={styles["nav-navbar-collapse"]}>
-          <Link
-            to={"https://www.nasa.gov/"}
-            className={styles["nav-item-nasa"]}
-            rel="noreferrer"
-            target="_blank"
-          >
-            NASA
-          </Link>
-          <button
-            className={styles["nav-item-pod"]}
-            onClick={() => [
-              dispatch(setToShowPoD(!globalData.showPoD)),
-              dispatch(setIsLoading(true)),
-            ]}
-          >
-            Picture of the day
-          </button>
-        </div>
-        <div
-          className={styles["nav-toggler-contaier"]}
-          onClick={() => checkIfMobile()}
-        >
-          <BiDotsHorizontalRounded />
-        </div>
-      </nav>
-    </div>
+    </section>
   );
 };
 
