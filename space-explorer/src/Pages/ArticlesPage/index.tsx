bdc3c502e0c18f799aa21e0973ee2e24e4f8ff7a
Martin B <m.bogdanov9110@gmail.com>
2023-08-28 16:41:16 +0300

    NEW Articles: -Created ArticlesPage and added logic  to it for visualising ArticleCards -Added ref for the active element + useIntersectionHook for the same logic

--- space-explorer/src/Pages/ArticlesPage/index.tsx
+++ space-explorer/src/Pages/ArticlesPage/index.tsx
@@ -1,38 +1,54 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import useIntersectionHook from "../../customHooks/useIntersectionHook";
 import styles from "./index.module.scss";
-import { ArticleCard } from "../../all-imported-components";
+import { ArticleCard, ErrorMessage } from "../../all-imported-components";
 import { useAppSelector } from "../../App/hooks";
-import { imageState } from "../../redux-slices/imagesSlice";
-import { TImageData } from "../../Interfaces and types/Types/types";
+
+import { TArticleItem } from "../../Interfaces and types/Types/types";
+import SearchForm from '../../Components/ImageSearchForm/SearchForm';
+import { articleState } from "../../redux-slices/articleSlice";
+import { globalState } from "../../redux-slices/globalSlice";
 
 const ArticlesPage = () => {
   const divRef = useRef<HTMLDivElement>(null);
   const isActive = useIntersectionHook(divRef, "#articles");
 
-  const imageData = useAppSelector(imageState);
+  const articleData = useAppSelector(articleState);
+  const globalData = useAppSelector(globalState);
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [, setToDisableLoadButton] = useState<boolean>(false);
 
   return (
     <section id="articles" className={styles["articles-main-container"]}>
+      <div className={styles["trigger"]} ref={divRef}></div>
       <div className={styles[isActive ? 'active-header-container' : 'inactive-header-container']}
       >
         <h2 className={styles["header"]}>
           Articles
         </h2>
+        <div>
+          <SearchForm {...{ setToDisableLoadButton, pageView: 'articles' }} />
+        </div>
         <p className={styles["sub-text"]}>
           The more you know
         </p>
       </div>
-      <div ref={divRef} className={styles["articles-secondary-container"]}>
+      <div className={styles["articles-secondary-container"]}>
         {
-          imageData?.allData[0]?.href !== ""
-            ?
-            imageData?.allData?.map((el: TImageData) => {
-              return (
-                <ArticleCard item={el} key={el.data[0].nasa_id} />
-              );
-            })
-            : null
+          globalData.error.error && globalData.error.page === "articles" || globalData.error.page === "modal"
+            ? <div className={styles["loader-error"]}>
+              <ErrorMessage error={globalData.error.error} />
+            </div>
+            :
+            articleData.results?.length
+              ?
+              articleData.results.map((el: TArticleItem) => {
+                return (
+                  <ArticleCard {...el} key={el.id ? Math.random() * el.id : Math.random() * 1000 / 5} />
+                );
+              })
+              : null
         }
       </div>
     </section >
