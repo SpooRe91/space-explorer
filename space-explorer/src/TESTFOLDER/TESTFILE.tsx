589f3d220be6a8e5adf9406ee88e5bd43aa86542
Martin B <85784810+SpooRe91@users.noreply.github.com>
2024-02-08 17:58:04 +0200

    DELETE DATA2 (#6)

--- space-explorer/src/TESTFOLDER/TESTFILE.tsx
+++ space-explorer/src/TESTFOLDER/TESTFILE.tsx
@@ -1,15 +1,11 @@
 
 const TESTFILE = () => {
 
-    const data2 = 'data2';
     const data3 = 'data3';
 
 
     return (
         <>
-            <div>
-                {data2}
-            </div>
             <div>
                 {data3}
             </div>
