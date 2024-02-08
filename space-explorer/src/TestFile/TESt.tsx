dccc07e6f069c3bc360f28e5dec03cf29e80b215
Martin B <85784810+SpooRe91@users.noreply.github.com>
2024-02-08 18:05:48 +0200

    del-data-1 (#8)

---
+++ space-explorer/src/TestFile/TESt.tsx
@@ -0,0 +1,14 @@
+const TESt = () => {
+
+    const data2 = 'data2'
+    const data3 = 'data3'
+
+    return (
+        <div>
+            <div>{data2}</div>
+            <div>{data3}</div>
+        </div>
+    )
+}
+
+export default TESt
