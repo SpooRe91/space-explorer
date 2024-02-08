2ca9bbb9df17098de572077c02ba33aa7d589e5b
Martin B <m.bogdanov9110@gmail.com>
2024-02-08 17:53:18 +0200

    TESTING CASE

---
+++ space-explorer/src/TESTFOLDER/TESTFILE.tsx
@@ -0,0 +1,24 @@
+
+const TESTFILE = () => {
+
+    const data1 = 'data1';
+    const data2 = 'data2';
+    const data3 = 'data3';
+
+
+    return (
+        <>
+            <div>
+                {data1}
+            </div>
+            <div>
+                {data2}
+            </div>
+            <div>
+                {data3}
+            </div>
+        </>
+    )
+}
+
+export default TESTFILE
