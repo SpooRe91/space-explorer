e61fa89da7f68b5cd8fdff76f71d5e7541f2339e
Martin B <m.bogdanov9110@gmail.com>
2024-04-17 18:13:25 +0300

    chore: Added ErrorBoundary

--- space-explorer/src/main.tsx
+++ space-explorer/src/main.tsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './styles/index.scss'
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./styles/index.scss";
+import { Provider } from "react-redux";
 import { store } from "./App/store";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
+import ErrorBoundary from "./Components/ErrorBoundary/ErrorBoundary.tsx";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
+                <ErrorBoundary>
+                    <App />
+                </ErrorBoundary>
+            </Provider>
+        </BrowserRouter>
+    </React.StrictMode>
+);
