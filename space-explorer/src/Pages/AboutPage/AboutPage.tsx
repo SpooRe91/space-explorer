a0339dc5316a486359d97bac5141a0902c9e930f
Martin B <m.bogdanov9110@gmail.com>
2024-06-24 21:12:37 +0300

    fix/feat: Added small text in about page + fixed the styling

--- space-explorer/src/Pages/AboutPage/AboutPage.tsx
+++ space-explorer/src/Pages/AboutPage/AboutPage.tsx
@@ -1,4 +1,3 @@
-
 import { Link } from "react-router-dom";
 import styles from "./AboutPage.module.scss";
 import icon from "@SpaceExplorer/assets/icons/android-chrome-256x256.webp";
@@ -7,7 +6,6 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 export const AboutPage = () => {
-
     return (
         // IF THE CURRENT REF ELEMENT IS ACTIVE, IT WILL APPLY A SPECIFIC SYLE TO IT
         <section id="about" className={styles["about-container"]}>
@@ -35,6 +33,7 @@ export const AboutPage = () => {
                     </Link>
                 </p>
                 <div className={styles["about-links-container"]}>
+                    <p>Reach me</p>
                     <Link to={"https://github.com/SpooRe91"} target="_blank" rel="noopener">
                         <GitHubIcon />
                     </Link>
